ff6ea66d4a648501440f69ff3f00ba4a9775b620
Jordi Roig Prieto <jordi.roig.prieto@stress.ad.upcnet.es>
2012-02-21 16:33:45 +0100

    media list pagination and search

--- fatac/forms/browser/gestioMedias.py
+++ fatac/forms/browser/gestioMedias.py
@@ -20,18 +20,61 @@ class gestioMedias(BrowserView, funcionsCerca):
             self.urlDelete = "javascript:call('delete','"+itemId+"');"
             self.urlConvert = "javascript:call('convert','"+itemId+"');"
     
+    def getSearchText(self):
+        if "search" in self.request.form:
+            return self.request.form["search"]
+        else:
+            return "" 
+    
+    def getPag(self):
+        if "pag" in self.request.form and self.request.form["pag"] != "":
+            return self.request.form["pag"]
+        else:
+            return "0"
+        
+    def getPagPlus1(self):
+        if "pag" in self.request.form and self.request.form["pag"] != "":
+            pag = int(self.request.form["pag"])
+        else:
+            pag = 0
+        
+        return "$('#pagInput').val(" + str(pag + 1) + ");document.gestioMedias.submit();"
+        
+    def getPagMinus1(self):
+        if "pag" in self.request.form and self.request.form["pag"] != "":
+            pag = int(self.request.form["pag"])
+            if pag < 1: return None
+            return "$('#pagInput').val(" + str(pag - 1) + ");document.gestioMedias.submit();"
+        else:
+            return None
+        
+    
     def getMediaList(self):
         restUrl = self.retServidorRest()
+        search = ""
+        pag = ""
         if "action" in self.request.form:
+            search = "?s="
+            pag = "?pag="        
             action = self.request.form["action"]
             id = self.request.form["id"]
             if action == 'delete':
                 request(restUrl + '/media/'+id+'/delete')
             if action == 'convert':
                 request(restUrl + '/media/'+id+'/convert')
+                
+            if "search" in self.request.form:
+                search = search + self.request.form["search"]
+                pag = "&pag=";
+            else:
+                search = ""
+                
+            if "pag" in self.request.form:
+                pag = pag + self.request.form["pag"]
+            else:
+                pag = ""
         
-        
-        resp = request(restUrl + '/media/list')
+        resp = request(restUrl + '/media/list' + search + pag)
         jsonResult = resp.tee().read()
         jsonTree = json.loads(jsonResult)
         
