54c6b9f76984fb2277da8ee94345abc2ffdcf202
Ariel <ariel.von.barnekow@upcnet.es>
2012-06-18 12:01:44 +0200

    PEP

--- fatac/forms/browser/deleteObject.py
+++ fatac/forms/browser/deleteObject.py
@@ -10,15 +10,15 @@ class deleteObject(BrowserView, funcionsCerca):
         self.context = context
 
     __call__ = ViewPageTemplateFile('templates/deleteObject.pt')
-    
+
     class resultItem:
         value = None
         link = None
-        
+
         def __init__(self, value, link):
             self.value = value
-            self.link = link    
-    
+            self.link = link
+
     def locator(self):
         try:
             oid = self.request['objectId']
@@ -26,20 +26,20 @@ class deleteObject(BrowserView, funcionsCerca):
             loclist = loc.split(",")[:-1]
             if self.request.get('pos', None):
                 pos = self.request.form['pos']
-                loclist = loclist[:int(pos)+1]
+                loclist = loclist[:int(pos) + 1]
             else:
                 pos = None
-                        
+
             result = []
             idx = 0
             for l in loclist:
-                result.append(self.resultItem(l,'javascript:goToObject("'+l+'","'+str(idx)+'")'))
+                result.append(self.resultItem(l, 'javascript:goToObject("' + l + '","' + str(idx) + '")'))
                 idx = idx + 1
-                
+
             return result
         except KeyError:
             oid = self.request.form['objectId']
-            result = [self.resultItem(oid,'javascript:goToObject("'+oid+'","0")')]
+            result = [self.resultItem(oid, 'javascript:goToObject("' + oid + '","0")')]
             return result
 
     def render(self):
@@ -48,12 +48,12 @@ class deleteObject(BrowserView, funcionsCerca):
             resp = request(self.retServidorRest() + '/resource/' + oid + '/delete', method='DELETE')
 
             result = resp.tee().read()
-            
+
             try:
-                myformHtml = "<form name='myform'><input type='hidden' name='locator' value='"+self.request.form['loc']+"'></form> <div id='mydiv'></div>"
+                myformHtml = "<form name='myform'><input type='hidden' name='locator' value='" + self.request.form['loc'] + "'></form> <div id='mydiv'></div>"
             except KeyError:
                 myformHtml = ""
-            
+            # TODO delete (if exists) dummy object
             return "Object esborrat <br/><br/><a href='./inici'>Inici</a>" + myformHtml
         else:
             return 'Oops!'
