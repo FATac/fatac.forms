ba70474ba2486a7f8f96df939d10f05c4ea6add6
Corina Riba <corina.riba@upcnet.es>
2012-07-03 18:20:14 +0200

    Pasar traducciones a fatac.theme

--- fatac/forms/locales/translations.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-msgfmt -o ca/LC_MESSAGES/fatac.forms.mo  ca/LC_MESSAGES/fatac.forms.po
-msgfmt -o es/LC_MESSAGES/fatac.forms.mo  es/LC_MESSAGES/fatac.forms.po 
-msgfmt -o en/LC_MESSAGES/fatac.forms.mo  en/LC_MESSAGES/fatac.forms.po  
