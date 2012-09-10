1a55978fa42b33aebc1f4b8a2e7976a3204e3aa0
Laura Pérez Mayos <lpmayos@gmail.com>
2012-09-10 14:20:42 +0200

    afegim filtre per classes a la cerca d'objectes

--- fatac/forms/browser/searchForm.py
+++ fatac/forms/browser/searchForm.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 import deform
 import colander
 import json
@@ -21,71 +23,81 @@ class searchForm(BrowserView, funcionsCerca):
         return not self.isSelect()
 
     def classSelection(self):
+
         try:
-            return self.request.form['classSelect']
+            classSelect = self.request.form['classSelect']
         except KeyError:
             try:
-                return self.request['classSelect']
+                classSelect = self.request['classSelect']
             except KeyError:
-                return ''
+                classSelect = None
+
+        # el primer cop, self.request.form['classSelect'] = 'ac:Media'
+        # després: self.request['classSelect'] = ['ac:Media', 'ac:Media']
+        if classSelect is not None and type(classSelect).__name__ != 'str':
+            classSelect = classSelect[0]
+
+        return classSelect
 
     def getPagMinus1(self):
-	if self.getPag() == 0: return None
-	return "document.forms['deform'].page.value="+str(self.getPag()-1)+";$(document.forms['deform'].submit).click();"
+        if self.getPag() == 0:
+            return None
+        return "document.forms['deform'].page.value=" + str(self.getPag() - 1) + ";$(document.forms['deform'].submit).click();"
 
     def getPagPlus1(self):
-        return "document.forms['deform'].page.value="+str(self.getPag()+1)+";$(document.forms['deform'].submit).click();"
-
+        return "document.forms['deform'].page.value=" + str(self.getPag() + 1) + ";$(document.forms['deform'].submit).click();"
 
     def getPag(self):
-	pageValue = self.request.get('page', 0)
-	if pageValue == 0: pageValue = self.request.form.get('page', 0)
-	return int(pageValue)
-
+        pageValue = self.request.get('page', 0)
+        if pageValue == 0:
+            pageValue = self.request.form.get('page', 0)
+        return int(pageValue)
 
     def render(self):
-	pageValue = self.request.get('page', 0)
-	if pageValue == 0: pageValue = self.request.form.get('page', 0)
+        pageValue = self.request.get('page', 0)
+        if pageValue == 0:
+            pageValue = self.request.form.get('page', 0)
 
-	textValue = self.request.get('text', '')
+        textValue = self.request.get('text', '')
+
+        choices = self.arbreClasses()
 
         class Schema(colander.Schema):
-                text = colander.SchemaNode(
+            text = colander.SchemaNode(
                     colander.String(),
                     validator=colander.Length(max=100),
                     widget=deform.widget.TextInputWidget(size=60),
                     description='Enter some texit',
-		    default=textValue)
-		page = colander.SchemaNode(
-		    colander.Int(),
-		    widget = deform.widget.HiddenWidget(),
-		    default = pageValue)
+                    default=textValue)
+
+            classSelect = colander.SchemaNode(
+                           colander.String(),
+                           widget=deform.widget.SelectWidget(values=choices),
+                           default=self.classSelection())
+
+            page = colander.SchemaNode(
+                    colander.Int(),
+                    widget=deform.widget.HiddenWidget(),
+                    default=pageValue)
 
         schema = Schema()
 
         if self.isSelect():
             schema.add(colander.SchemaNode(
-                    colander.String(),
-                    widget=deform.widget.HiddenWidget(),
-                    name='objectSelect',
-                    default='true'
-                    ))
+                colander.String(),
+                widget=deform.widget.HiddenWidget(),
+                name='objectSelect',
+                default='true'
+                ))
 
-        try:
-            classSelection = self.request.form['classSelect']
-        except KeyError:
-            try:
-                classSelection = self.request['classSelect']
-            except KeyError:
-                classSelection = None
-
+        classSelection = self.classSelection()
         if classSelection is not None:
             schema.add(colander.SchemaNode(
-                    colander.String(),
-                    widget=deform.widget.HiddenWidget(),
-                    name='classSelect',
-                    default=classSelection
-                    ))
+                colander.String(),
+                widget=deform.widget.HiddenWidget(),
+                name='classSelect',
+                default=classSelection
+                ))
 
         form = deform.Form(schema, action='seek', buttons=[deform.Button('submit', 'Buscar')])
         return form.render()
@@ -95,15 +107,14 @@ class searchForm(BrowserView, funcionsCerca):
         value = None
         link = None
         legalLink = None
-	fitxaLink = None
+        fitxaLink = None
 
         def __init__(self, name, value, link, legalLink, fitxaLink):
             self.name = name
             self.value = value
             self.link = link
             self.legalLink = legalLink
-	    self.fitxaLink = fitxaLink
-
+            self.fitxaLink = fitxaLink
 
     def results(self):
         result = list()
@@ -118,13 +129,16 @@ class searchForm(BrowserView, funcionsCerca):
                 usrId = ''
 
             self.text = self.request['text']
-	    self.page = self.getPag()
+            self.page = self.getPag()
 
-            classSelection = ''
-            if not vIsSearch:
-                classSelection = "&c=" + self.request.form['classSelect']
+            classe_aux = self.classSelection()
+            classe = ''
+            if classe_aux != 'Totes':
+                classe = "&c=" + classe_aux
 
-            resp = request(self.retServidorRest() + '/search?s=' + str.replace(self.text, " ", "+") + classSelection + "&page=" + str(self.page))
+            self.context.plone_log('serahcForm.py: ' + self.retServidorRest() + '/search?s=' + str.replace(self.text, " ", "+") + classe + "&page=" + str(self.page))
+            resp = request(self.retServidorRest() + '/search?s=' + str.replace(self.text, " ", "+") + classe + "&page=" + str(self.page))
+
             jsonResult = resp.tee().read()
             jsonTree = json.loads(jsonResult)
 
@@ -135,6 +149,44 @@ class searchForm(BrowserView, funcionsCerca):
                 if vIsSearch:
                     result.append(self.resultItem('', '', './updateExisting?id=' + s, './legalValidation?objectIdsVal=' + s, './genericView?idobjecte=' + s))
                 else:
-                    result.append(self.resultItem('', '', 'javascript:opener.setCurrentInputValue("'+s+'"); window.close();', None, None))
+                    result.append(self.resultItem('', '', 'javascript:opener.setCurrentInputValue("' + s + '"); window.close();', None, None))
 
         return result
+
+    def llistaClassesPrincipals(self, jsonTree):
+        """  parseja l'arbre de classes jsonTree i retorna una llista amb les
+        classes principals (presuposant que l'element 'pare' és 'owl:Thing'
+        """
+
+        llista_classes = []
+
+        if 'owl:Thing' in jsonTree:
+            classes = jsonTree['owl:Thing']
+            for classe in classes:
+                if type(classe).__name__ == 'dict':
+                    for e in classe.keys():
+                        llista_classes.append(e)
+
+                        #afegim també les classes de segon nivell
+                        for subclasse in classe[e]:
+                            if type(subclasse).__name__ == 'dict':
+                                for ee in subclasse.keys():
+                                    llista_classes.append('###' + ee)
+                            else:
+                                llista_classes.append('_____' + subclasse)
+                else:
+                    llista_classes.append(classe)
+        return llista_classes
+
+    def arbreClasses(self):
+        """ crida el servei self.retServidorRest() + '/classes/tree' i retorna
+        el resultat en forma de tupla per passarl-li al widget de deform, afegint
+        una primera opció 'Totes'
+        """
+
+        resp = request(self.retServidorRest() + '/classes/tree')
+        jsonResult = resp.tee().read()
+        jsonTree = json.loads(jsonResult)
+
+        llista = ['Totes'] + self.llistaClassesPrincipals(jsonTree)
+        return tuple([(a.replace('_____', ''), a) for a in llista])
