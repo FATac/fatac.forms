1fbbbc712e251dfaa6feee2c5e6bc2a523ccfef8
Pilar Marinas <pilar.marinas@upcnet.es>
2013-06-18 15:19:55 +0200

    Comentat gestionarLlibre quan es crea ja que fins que no arriva al solr no hi ha info

--- fatac/forms/browser/uploadObject.py
+++ fatac/forms/browser/uploadObject.py
@@ -33,7 +33,7 @@ class uploadObject(BrowserView, funcionsCerca):
                 if fieldName in self.request:
                     fieldValue = self.request[fieldName]
 
-                    #Com quan crees el llibre fins que no arriva al solr no es veu peta, per tal ho comentem
+                    # Com quan crees el llibre fins que no arriva al solr no es la informacio, ho comentem
                     # if fieldName == u'DisplayScreen' and fieldValue == 'on':
                     #     GestionarLlibre = True 
                     if fieldName + '_lang' in self.request:
@@ -74,11 +74,13 @@ class uploadObject(BrowserView, funcionsCerca):
             self.newId = result
 
             if result != 'error':
+                #forcem la visualitzacio del objecte perque no doni error si vas a opcio Gestionar Llibre directament
+                #fitxa = request(self.context.portal_url()+'/genericView?idobjecte=%s' % self.request.form['about'])
                 if '__relatedObject' in self.request:
                     return "&nbsp;<a href='javascript:opener.setCurrentInputValue(\"" + result + "\"); opener.focus(); window.close();'>[Seleccionar]</a>"
                 #Com quan crees el llibre fins que no arriva al solr no es veu peta, per tal ho comentem
                 # elif GestionarLlibre:
-                #     return "Objecte desat correctament. <br/><br/><a href='./updateExisting?id=" + result + "'>Edita</a><span class='separator'> | </span><a href='./legalValidation?objectIdsVal=" + result + "'>Legal</a><span class='separator'> | </span><a target='_blank' href='./genericView?idobjecte=" + result + "'>Fitxa</a><span class='separator'> | </span><a target='_blank' href='./ac/" + result + "'/gestionarLlibre'>Gestionar Llibre</a>"
+                #     return "Objecte desat correctament. <br/><br/><a href='./updateExisting?id=" + result + "'>Edita</a><span class='separator'> | </span><a href='./legalValidation?objectIdsVal=" + result + "'>Legal</a><span class='separator'> | </span><a target='_blank' href='./genericView?idobjecte=" + result + "'>Fitxa</a><span class='separator'> | </span><a target='_blank' href='./ac/" + result + '/gestionarLlibre'"'>Gestionar Llibre</a>"
                 else:
                     # return "<a href='./genericView?idobjecte=" + result + "' target='_fitaWindow'>Veure fitxa</a>"
                     return "Objecte desat correctament. <br/><br/><a href='./updateExisting?id=" + result + "'>Edita</a><span class='separator'> | </span><a href='./legalValidation?objectIdsVal=" + result + "'>Legal</a><span class='separator'> | </span><a target='_blank' href='./genericView?idobjecte=" + result + "'>Fitxa</a>"
